57920cfe75fb4c37febcb34fcaeff65e2a45ce7b
wisleysiqueira <wisleysiqueira@hotmail.com>
2018-08-19 22:22:50 -0300

    atualizando projeto

--- class/usuario.php
+++ class/usuario.php
@@ -1,8 +1,33 @@
 <?php
 	class usuario{
-		private $idusuarios;
+		private $idusuario;
 		private$deslogin;
 		private $dessenha;
 		private $dtcadastro;
+
+		public function getIdusuario(){
+			return $this->idusuarios;
+		}
+		public function setIdusuario($value){
+			$this->idusuario=$value;
+		}
+		public function getDeslogin(){
+			return $this->deslogin;
+		}
+		public function setDeslogin($value){
+			$this->deslogin=$value;
+		}
+		public function getDessenha(){
+			return $this->dessenha;
+		}
+		public function setDessenha($value){
+			$this->dessenha=$value;
+		}
+		public function getDtcadastro(){
+			return $this->dtcadastro;
+		}
+		public function setDtcadastro($value){
+			$this->dtcadastro=$value;
+		}
 	}
 ?>
